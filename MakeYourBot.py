1da278125457de5de25191c59286bc2bfc3f38fa
bubudotsh <bunelierpro@gmail.com>
2022-05-18 19:40:15 +0200

    test os detection

--- MakeYourBot.py
+++ MakeYourBot.py
@@ -1,4 +1,10 @@
+from curses import echo
 import shutil
+import platform
+
+def osdetect ():
+    osname = platform.system()
+    print(osname)
 
 
 def prefix_engine(botname, botprefix):
@@ -21,11 +27,34 @@ def file_copy(botname):
     destination_dir = r"botgen/" + botname
     shutil.copytree(source_dir, destination_dir)
 
+def commandes ():
+    print("addrole")
+    print("ban")
+    print("botinfo")
+    print("botinfo")
+    print("clear")
+    print("clearwarn")
+    print("coinflip")
+
+def choose_cmds ():
+    print("type name commande you want : ")
+    cmds = input()
+    while not (cmds == 'next'):
+        commandes()
+        print("type name commande you want : ")
+        cmds = input()
+        print("your cmds : " + cmds)
+
 
 print("MakeYourBot\n")
+osdetect()
+
 print("name of yout bot :")
 botname = input()
 file_copy(botname)
 print("prefix of your bot :")
 botprefix = input()
 prefix_engine(botname, botprefix)
+print("your bot" + botname + "was create ! Now choose your commande")
+commandes()
+choose_cmds()
